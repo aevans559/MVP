f9adf9e0f73703c27937526804e06f777343eaf7
Aaron Evans <aaronmevans14@gmail.com>
2020-03-20 15:10:29 -0500

    added ability to filter by xbox or playstation

--- client/src/components/App/App.jsx
+++ client/src/components/App/App.jsx
@@ -10,17 +10,19 @@ export default class App extends Component {
                 wins: 0,
                 kills: 0
             },
-            gtag: ''
+            gtag: '',
+            psn: false,
+            xbl: false
         }
         this.callAPI = this.callAPI.bind(this)
         this.handleSubmit = this.handleSubmit.bind(this)
         this.handleChange = this.handleChange.bind(this)
+        this.handleCheckboxChange = this.handleCheckboxChange.bind(this)
     }
 
     callAPI() {
-        axios.get(`http://localhost:5500/gtag/${this.state.gtag}`)
+        axios.get(`http://localhost:5500/${this.state.psn ? 'psn' : this.state.xbl ? 'xbl' : null }/${this.state.gtag}`)
             .then(res => {
-                // const codstats = res.data
                 console.log(res.data)
                 this.setState({ codstats: {
                     wins: res.data.lifetime.mode.br.properties.wins,
@@ -36,21 +38,58 @@ export default class App extends Component {
 
     handleChange(e) {
         this.setState({gtag: e.target.value})
+        e.preventDefault();
     }
 
     handleSubmit(e) {
         this.callAPI();
-        console.log(this.state.gtag)
         e.preventDefault();
+    }
+
+    handleCheckboxChange(e) {
+        e.target.name === 'psn' ? 
+        this.setState({ 
+            psn: true
+        }) :
+        e.target.name === 'xbl' ?
+        this.setState({ 
+            xbl: true
+        }) : null
     }
 
     render() {
         return (
             <div className={styles.App}>
                 <form onSubmit={this.handleSubmit}>
+                    <label>
+                    PSN
+                    <input
+                        type="checkbox"
+                        name="psn"
+                        value="psn"
+                        onChange={this.handleCheckboxChange}
+                    />
+                    </label>
+                    <label>
+                    Xbox Live
+                    <input 
+                        type="checkbox"
+                        name="xbl"
+                        value="xbl"
+                        onChange={this.handleCheckboxChange}
+                    />
+                    </label>
+                    <br/>
                     <label>Gamertag:</label>
-                    <input type="text" value={this.state.value} onChange={this.handleChange} id="gtag" name="gtag" required="required"></input>
-                    <button type='submit'>Search</button>
+                    <input 
+                        type="text"
+                        value={this.state.value}
+                        onChange={this.handleChange}
+                        id="gtag"
+                        name="gtag"
+                        required="required">
+                    </input>
+                    <button type="submit">Search</button>
                 </form>
                 <div className={styles.userInfo}>
                 <p>{this.state.codstats.username}</p>
--- server/server.js
+++ server/server.js
@@ -4,7 +4,8 @@ const app = express();
 const cors = require('cors');
 const path = require('path');
 const port = 5500;
-const COD = require('call-of-duty-api')(({platform: 'psn'}));
+const COD_PSN = require('call-of-duty-api')(({platform: 'psn'}));
+const COD_XBL = require('call-of-duty-api')(({platform: 'xbl'}));
 require('dotenv').config();
 
 // app.use(bodyParser.urlencoded({extended: false}));
@@ -18,8 +19,18 @@ app.get('/', (req, res) => {
     res.send('hello from server')
 });
 
-app.get('/gtag/:id', (req, res) => {
-    COD.MWstats(req.params.id, COD.platforms.psn)
+// route for playstation users
+app.get('/psn/:gtag', (req, res) => {
+    COD_PSN.MWstats(req.params.gtag, COD_PSN.platforms.psn)
+        .then((output) => {
+            res.json(output)
+        })
+        .catch(err => console.log(err))
+})
+
+// route for xbox users
+app.get('/xbl/:gtag', (req, res) => {
+    COD_XBL.MWstats(req.params.gtag, COD_XBL.platforms.xbl)
         .then((output) => {
             res.json(output)
         })
